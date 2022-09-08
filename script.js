56f38e06ff63e00f309a264fb7b519720b839b80
Julio Quezada <95459740+Alejandroq12@users.noreply.github.com>
2022-09-08 17:07:29 -0600

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+const { without } = require('lodash');
+var _ = require('lodash');
+
+var array = [1,2,3,4,5,6,7, 8];
+console.log('answer:', _.without(array, 3))
+
+var css = document.querySelector("h3");
+var color1 = document.querySelector(".color1");
+var color2 = document.querySelector(".color2");
+var body = document.getElementById("gradient");
+
+function setGradient() {
+	body.style.background = 
+	"linear-gradient(to right, " 
+	+ color1.value 
+	+ ", " 
+	+ color2.value 
+	+ ")";
+
+	css.textContent = body.style.background + ";";
+}
+
+color1.addEventListener("input", setGradient);
+
+color2.addEventListener("input", setGradient);
